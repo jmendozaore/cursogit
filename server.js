5032ec6a3f0cb98e27643b1fdfe2ca798e7b2fe7
jmendozaore <jmendozaore@gmail.com>
2025-01-04 13:05:40 -0500

    server update 2

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const hbs = require('hbs');
 const app = express()
+const PORT = 3000
 
+app.set('view engine', 'hbs');
 app.get('/', function (req, res) {
-  res.send('Bienvenido a nuestro primer Servidor con Express')
+    res.send('Bienvenidos a nuestro Primer Servidor con Express')
 })
-
-app.listen(3000)
+app.get('/navidad', function (req, res) {
+    res.render('index')
+});
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
+    
+})
